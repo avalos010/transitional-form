bc4e9a09278762013d228145ce74c1f8aa22cc1e
avalos010 <luizavalos40@gmail.com>
2021-01-25 13:08:32 -0600

    simplified event listener function

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-document.getElementById('fname-btn').addEventListener('click', function() {
-    document.querySelector('.fname').classList.add('hide')
-    document.querySelector('.lname').classList.remove('hide')
-    document.querySelector('.lname').classList.add('show')
+document.getElementById('fname-btn').addEventListener('click', () => transition('fname'))
+document.getElementById('lname-btn').addEventListener('click', () => transition('lname'))
 
-})
-
-document.getElementById('lname-btn').addEventListener('click', function() {
-    document.querySelector('.lname').classList.add('hide')
-    document.querySelector('.email').classList.remove('hide')
-    document.querySelector('.email').classList.add('show')
-})
-
+function transition(el) {
+    switch(el) {
+        case 'lname': 
+            document.querySelector('.lname').classList.add('hide')
+            document.querySelector('.email').classList.remove('hide')
+            document.querySelector('.email').classList.add('show')
+            break;
+        case 'fname':
+            document.querySelector('.fname').classList.add('hide')
+            document.querySelector('.lname').classList.remove('hide')
+            document.querySelector('.lname').classList.add('show')
+            break;
+         default:
+            return null
+    }   
+}
